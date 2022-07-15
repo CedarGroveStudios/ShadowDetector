d8940ee7d40e1622e6302cd935ab6d29eb1cdd10
Cedar Grove Maker Studios <jan@goolsbey.net>
2022-07-15 12:53:51 -0700

    Update docs

--- shadow_detector_example.py
+++ shadow_detector_example.py
@@ -4,11 +4,14 @@ import board
 import time
 from shadow_detector import ShadowDetector
 
+# Instantiate detector class
 gesture = ShadowDetector(pin=board.LIGHT)
 
 while True:
     if gesture.detect():
         print(f"SHADOW DETECTED")
         while gesture.detect():
+            # Wait until the shadow is gone
             time.sleep(1)
+        # Rebaseline the background level
         gesture.refresh_background()
